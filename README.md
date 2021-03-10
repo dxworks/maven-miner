533068b7d84ba9bc67a5d497ff1d462522b09348
Mario Rivis <mario.rivis@gmail.com>
2021-03-10 18:01:20 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MavenMiner
+
+MavenMiner, sometimes also called MaMi,is a tool which extracts the basic model of your maven projects. It also creates a graph of relations between the internal modules of your project.
+
+# Running the tool
+Run `java -jar maven-miner.jar <path_to_folder>`, where `<path_to_folder>` represents the fully qualified path of the folder containing all the source code you want MavenMiner to analyze. If you have multiple repositories you want to analyze together, please group them under a single _root folder_, and pass this as an argument to maven-miner. 
+
+This will generate in the `results` folder 3 files:
+* `maven-graph.json` ---> a graph representation of your maven modules
+* `maven-model.json` ---> a simplified model of your maven modules
+* `maven-relations.csv` ---> a csv with the relations between the modules.
---
+++ src/main/java/org/dxworks/mavenminer/Main.kt
@@ -0,0 +1,146 @@
+package org.dxworks.mavenminer
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.apache.maven.model.Model
+import org.apache.maven.model.Parent
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.io.path.*
+import kotlin.streams.toList
+
+@OptIn(ExperimentalPathApi::class)
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        error("Bad arguments! Please provide only one argument, namely the path to the folder containing the source code.")
+    }
+
+    val baseFolderArg = args[0]
+
+    val baseFolder = File(baseFolderArg)
+
+    val baseFolderPath = baseFolder.toPath()
+    val poms = Files.walk(baseFolderPath)
+            .filter { it.isRegularFile() }
+            .filter { it.name == "pom.xml" }
+            .toList()
+
+    val mavenReader = MavenXpp3Reader()
+
+    val modulesMap = poms
+            .mapNotNull { path ->
+                val mavenModel = mavenReader.read(path.inputStream())
+                val mavenModuleId = extractMavenModuleId(mavenModel)
+                val deps = mavenModel.dependencies.mapNotNull { MavenDependency(it.groupId, it.artifactId, it.version, it.isOptional) }
+                MavenModule(mavenModuleId, baseFolderPath.relativize(path), mavenModel.parent?.let { MavenParent(it) }, deps, mavenModel.properties)
+            }
+            .map { it.id to it }
+            .toMap()
+
+    val nodes = modulesMap.keys.map { GraphNode(it.artifactID) }
+    val links = modulesMap.values.map { module ->
+        module.dependencies
+                .mapNotNull { modulesMap[it.toModuleId()] }
+                .map { GraphLink(module.id.artifactID, it.id.artifactID) }
+    }.flatten().toList()
+
+    Path.of("results").toFile().mkdirs()
+
+    val modelPath = Path.of("results", "maven-model.json")
+    val graphPath = Path.of("results", "maven-graph.json")
+    val relationsPath = Path.of("results", "maven-relations.csv")
+
+    jacksonObjectMapper().writerWithDefaultPrettyPrinter().writeValue(modelPath.toFile(), modulesMap.values)
+    jacksonObjectMapper().writerWithDefaultPrettyPrinter().writeValue(graphPath.toFile(), Graph(nodes, links))
+
+    relationsPath.writeLines(links.map { "${it.source},${it.target},${it.value}" })
+
+}
+
+fun extractMavenModuleId(mavenModel: Model): MavenModuleId {
+    return MavenModuleId(mavenModel.groupId ?: mavenModel.parent?.groupId,
+            mavenModel.artifactId,
+            mavenModel.version ?: mavenModel.parent?.version)
+}
+
+data class MavenModule(
+        val id: MavenModuleId,
+        @JsonIgnore
+        val path: Path,
+        val parent: MavenParent? = null,
+        var dependencies: List<MavenDependency> = ArrayList(),
+        val properties: Properties
+//        val mavenModel: Model,
+) {
+    @JsonProperty("path")
+    val relativePath = path.toString()
+}
+
+open class MavenModuleId(
+        val groupID: String?,
+        val artifactID: String,
+        val version: String?,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MavenModuleId
+
+        if (groupID != other.groupID) return false
+        if (artifactID != other.artifactID) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = groupID?.hashCode() ?: 0
+        result = 31 * result + artifactID.hashCode()
+        return result
+    }
+}
+
+class MavenParent(groupID: String?, artifactID: String, version: String?, val relativePath: String? = null)
+    : MavenModuleId(groupID, artifactID, version) {
+    constructor(parent: Parent) : this(parent.groupId, parent.artifactId, parent.version, parent.relativePath)
+}
+
+data class MavenDependency(
+        val groupID: String?,
+        val artifactID: String,
+        val version: String?,
+        val optional: Boolean,
+) {
+    fun toModuleId() = MavenModuleId(groupID, artifactID, version)
+}
+
+data class Graph(
+        val nodes: List<GraphNode>,
+        val links: List<GraphLink>
+)
+
+data class GraphNode(
+        val name: String,
+        val component: Number = 1
+)
+
+data class GraphLink(
+        val source: String,
+        val target: String,
+        val value: Number = 1
+)
+
+data class Result(
+        val name: String,
+        val description: String,
+        val visualTags: List<String>,
+        val entity: String,
+        val timestamp: Long,
+        val content: Graph
+)
+
